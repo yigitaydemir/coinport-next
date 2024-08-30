8dc591d5316ec0d9dab04fecf65cb21c85244b72
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-08-30 23:06:21 +0300

    devam

--- lesson03/build/js/test.js
+++ lesson03/build/js/test.js
@@ -1,1 +1,88 @@
 "use strict";
+// let myName: string = "Yiğit";
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// };
+// let myNumber: string | number;
+// myNumber = "05324008090"
+// let re: RegExp = /\w+/g
+// let stringArr: string[] = ["deneme", "test", "main"]
+// let guitars: (string | number)[] = ["strat", "les Paul", 5150]
+// let mixedData: any[] = ["abc", 123, true, false, {test: "test"}]
+// stringArr[0] = "mehmet"
+// stringArr.push("yiğit")
+// console.log(stringArr)
+// tuple
+let myTuple = ["yiğit", 123, true];
+//obj
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+const exampleObj = {
+    prop1: "yiğit",
+    prop2: "25",
+};
+let evh = {
+    name: "eddie",
+    active: false,
+    albums: [1984, "test"],
+};
+const greetArtist = (artis) => {
+    if (artis.name) {
+        return `Hello ${artis.name.toUpperCase()}!`;
+    }
+    return "Hello!";
+};
+console.log(greetArtist(evh));
+// enums - unlike most typescript features, enums
+// are not a type-level addition to javascript but
+// something added to the language and runtime
+var Grade;
+(function (Grade) {
+    Grade[Grade["VF"] = 0] = "VF";
+    Grade[Grade["FF"] = 1] = "FF";
+    Grade[Grade["DD"] = 2] = "DD";
+    Grade[Grade["DC"] = 3] = "DC";
+    Grade[Grade["CC"] = 4] = "CC";
+    Grade[Grade["CB"] = 5] = "CB";
+    Grade[Grade["BB"] = 6] = "BB";
+    Grade[Grade["BA"] = 7] = "BA";
+    Grade[Grade["AA"] = 8] = "AA";
+})(Grade || (Grade = {}));
+console.log(Grade.FF);
+// literal types
+let myName;
+let userName;
+userName = "dave";
+// void -> functions that dont return anything
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg("my func");
+// interface mathFunction {
+//     (a: number, b: number): number
+// }
+let multiply = function (c, d) {
+    return c * d;
+};
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+// default parameters
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+logMsg(sumAll(undefined, 3));
+// rest parameters
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, current) => prev + current);
+};
+logMsg(total(1, 2, 3, 4, 5, 6));
+// type assertion - type casting
--- lesson03/src/main.ts
+++ lesson03/src/main.ts
@@ -140,7 +140,7 @@
 //     model: "Corolla",
 //     year: 2009
 //   };
-  
+
 // Optional Properties
 // Optional properties are properties that don't have to be defined in the object definition.
 
@@ -171,7 +171,7 @@
 //   console.log(currentDirection);
 //   // throws error as 'North' is not a valid enum
 //   currentDirection = 'North'; // Error: "North" is not assignable to type 'CardinalDirections'.
-  
+
 //   Numeric Enums - Initialized
 // You can set the value of the first numeric enum and have it auto increment from that:
 
@@ -219,3 +219,218 @@
 
 // Technically, you can mix and match string and numeric enum values, but it is recommended not to do so.
 
+// TypeScript Type Aliases and Interfaces
+
+// TypeScript allows types to be defined separately from the variables that use them.
+
+// Aliases and Interfaces allows types to be easily shared between different variables/objects.
+
+// Type Aliases
+// Type Aliases allow defining types with a custom name (an Alias).
+
+// Type Aliases can be used for primitives like string or more complex types such as objects and arrays:
+
+// Example
+
+// type CarYear = number
+// type CarType = string
+// type CarModel = string
+// type Car = {
+//   year: CarYear,
+//   type: CarType,
+//   model: CarModel
+// }
+
+// const carYear: CarYear = 2001
+// const carType: CarType = "Toyota"
+// const carModel: CarModel = "Corolla"
+// const car: Car = {
+//   year: carYear,
+//   type: carType,
+//   model: carModel
+// };
+
+// Interfaces
+// Interfaces are similar to type aliases, except they only apply to object types.
+
+// Interfaces
+// Interfaces are similar to type aliases, except they only apply to object types.
+
+// Example
+// interface Rectangle {
+//   height: number,
+//   width: number
+// }
+
+// const rectangle: Rectangle = {
+//   height: 20,
+//   width: 10
+// };
+
+// Extending Interfaces
+// Interfaces can extend each other's definition.
+
+// Extending an interface means you are creating a new interface with the same properties as the original, plus something new.
+
+// Example
+// interface Rectangle {
+//   height: number,
+//   width: number
+// }
+
+// interface ColoredRectangle extends Rectangle {
+//   color: string
+// }
+
+// const coloredRectangle: ColoredRectangle = {
+//   height: 20,
+//   width: 10,
+//   color: "red"
+// };
+
+// TypeScript Union Types
+// Union types are used when a value can be more than a single type.
+
+// Such as when a property would be string or number.
+
+// Using the | we are saying our parameter is a string or number:
+
+// function printStatusCode(code: string | number) {
+//     console.log(`My status code is ${code}.`)
+//   }
+//   printStatusCode(404);
+//   printStatusCode('404');
+
+// TypeScript Functions
+
+// TypeScript has a specific syntax for typing function parameters and return values.
+
+// Return Type
+// The type of the value returned by the function can be explicitly defined.
+
+// // the `: number` here specifies that this function returns a number
+// function getTime(): number {
+//     return new Date().getTime();
+// }
+
+// If no return type is defined, TypeScript will attempt to infer it through the types of the variables or expressions returned.
+
+// Void Return Type
+// The type void can be used to indicate a function doesn't return any value.
+
+// Example
+// function printHello(): void {
+//   console.log('Hello!');
+// }
+
+// Parameters
+// Function parameters are typed with a similar syntax as variable declarations.
+
+// Example
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+
+// If no parameter type is defined, TypeScript will default to using any, unless additional type information is available as shown in the Default Parameters and Type Alias sections below.
+
+// Optional Parameters
+// By default TypeScript will assume all parameters are required, but they can be explicitly marked as optional.
+
+// Example
+// // the `?` operator here marks parameter `c` as optional
+// function add(a: number, b: number, c?: number) {
+//   return a + b + (c || 0);
+// }
+
+// Default Parameters
+// For parameters with default values, the default value goes after the type annotation:
+
+// Example
+// function pow(value: number, exponent: number = 10) {
+//   return value ** exponent;
+// }
+
+// Rest Parameters
+// Rest parameters can be typed like normal parameters, but the type must be an array as rest parameters are always arrays.
+
+// Example
+// function add(a: number, b: number, ...rest: number[]) {
+//   return a + b + rest.reduce((p, c) => p + c, 0);
+// }
+
+// Type Alias
+// Function types can be specified separately from functions with type aliases.
+
+// These types are written similarly to arrow functions, read more about arrow functions here.
+
+// Example
+// type Negate = (value: number) => number;
+
+// // in this function, the parameter `value` automatically gets assigned the type `number` from the type `Negate`
+// const negateFunction: Negate = (value) => value * -1;
+
+// TypeScript Casting
+// There are times when working with types where it's necessary to override the type of a variable, such as when incorrect types are provided by a library.
+
+// Casting is the process of overriding a type.
+
+// Casting with as
+// A straightforward way to cast a variable is using the as keyword, which will directly change the type of the given variable.
+
+// Example
+// let x: unknown = 'hello';
+// console.log((x as string).length);
+
+// Casting doesn't actually change the type of the data within the variable, for example the following code will not work as expected since the variable x is still holds a number.
+// let x: unknown = 4;
+// console.log((x as string).length); // prints undefined since numbers don't have a length
+
+// Casting with <>
+// Using <> works the same as casting with as.
+
+// Example
+// let x: unknown = 'hello';
+// console.log((<string>x).length);
+
+// This type of casting will not work with TSX, such as when working on React files.
+
+// Force casting
+// To override type errors that TypeScript may throw when casting, first cast to unknown, then to the target type.
+
+// Example
+// let x = 'hello';
+// console.log(((x as unknown) as number).length); // x is not actually a number so this will return undefined
+
+const stringEcho = (arg: string): string => {
+  return arg;
+};
+
+const echo = <T>(arg: T): T => arg;
+
+const isObj = <T>(arg: T): boolean => {
+  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+};
+
+console.log(isObj(true));
+console.log(isObj(1234));
+console.log(isObj("string"));
+console.log(isObj({ name: "yiğit" }));
+
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  return { arg, is: !!arg };
+};
+
+function createPair<S, T>(v1: S, v2: T): [S, T] {
+  return [v1, v2];
+}
+console.log(createPair<string, number>("hello", 42)); // ['hello', 42]
+
+// TypeScript can also infer the type of the generic parameter from the function parameters.
+
+// Type Aliases
+// Generics in type aliases allow creating types that are more reusable.
+
+// Example
+// type Wrapped<T> = { value: T };
+
+// const wrappedValue: Wrapped<number> = { value: 10 };
