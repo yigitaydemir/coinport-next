21876cf1e80ef721c5334f4f1a2281df393185d0
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-08-31 13:11:51 +0300

    devam

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import Heading from "./components/Heading"
-import Section from "./components/Section"
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+import Counter from "./components/Counter";
+import { useState } from "react";
+import List from "./components/List";
 
 const App = () => {
+  const [count, setCount] = useState<number>(1);
+
   return (
-    <>
+    <div style={{ backgroundColor: "gray" }}>
       <Heading title={"Hello"}></Heading>
       <Section title={"This is not default title"}>This is my section.</Section>
-    </>
-  )
-}
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["coffee", "tacos", "code"]}
+        render={(item: string) => <span style={{ color: "gold" }}>{item}</span>}
+      ></List>
+    </div>
+  );
+};
 
-export default App
+export default App;
