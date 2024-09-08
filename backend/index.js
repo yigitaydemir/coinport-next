d9c3030aae41209c1e5092c0d0988aac73f022c2
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-09-09 00:01:46 +0300

    create, read, update and delete features added

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,81 @@
-import express from "express"
-import mysql from "mysql2"
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "yusuf..99",
-    database: "test"
-})
+  host: "localhost",
+  user: "root",
+  password: "yusuf..99",
+  database: "test",
+});
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json("hello this is backend")
-})
+  res.json("hello this is backend");
+});
 
 app.get("/books", (req, res) => {
-    const q = "SELECT * FROM books"
+  const q = "SELECT * FROM books";
 
-    db.query(q, (err, data) => {
-        if (err) return res.json(err)
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
 
-        return res.json(data)
-    })
-})
+    return res.json(data);
+  });
+});
 
 app.post("/books", (req, res) => {
-    const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)"
+  const q =
+    "INSERT INTO books (`title`, `description`, `cover`, `price`) VALUES (?)";
 
-    const values = ["title from backend", "desc from backend", "cover pic from backend"]
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.cover,
+    req.body.price,
+  ];
 
-    db.query(q, [values], (err, data) => {
-        if (err) return res.json(err)
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
 
-        return res.json(data)
-    })
-})
+    return res.json("book created");
+  });
+});
+
+app.put("books/:id", (req, res) => {
+  const bookId = req.params.id;
+
+  const q =
+    "UPDATE books SET `title` = ?, `descrition` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.cover,
+    req.body.price,
+  ];
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("book updated");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+
+  const q = "DELETE FROM books WHERE id = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("book is deleted");
+  });
+});
 
 app.listen(8800, () => {
-    console.log("connected to backend")
-})
+  console.log("connected to backend");
+});
---
+++ frontend/src/components/Update.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Update = () => {
+  const [book, setBook] = useState({
+    title: "",
+    description: "",
+    price: null,
+    cover: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.put("http://localhost:8800/books", book);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: "gray",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "25px",
+      }}
+    >
+      <h1>Update Book</h1>
+
+      <form style={{ border: "2px solid black", padding: "25px" }}>
+        <div style={{ margin: "5px 0" }}>
+          <label htmlFor="">Title</label>
+          <input type="text" name="title" id="" onChange={handleChange} />
+        </div>
+
+        <div style={{ margin: "5px 0" }}>
+          <label htmlFor="">Description</label>
+          <input type="text" name="description" onChange={handleChange} />
+        </div>
+
+        <div style={{ margin: "5px 0" }}>
+          <label htmlFor="">Cover</label>
+          <input type="text" name="cover" onChange={handleChange} />
+        </div>
+
+        <div style={{ margin: "5px 0" }}>
+          <label htmlFor="">Price</label>
+          <input type="number" name="price" onChange={handleChange} />
+        </div>
+
+        <button
+          onClick={handleClick}
+          style={{ marginTop: "10px", padding: "7px" }}
+        >
+          Update Book
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Update;
