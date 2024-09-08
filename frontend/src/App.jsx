69f36078215d9d20c39dbbf53df163b32f534e4a
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-09-08 18:46:03 +0300

    devam

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./Home"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <h1>asdasdasdsad</h1>
+      <h1>asdasdasdas</h1>
+    </div>
+  );
+};
+
+export default Home;
