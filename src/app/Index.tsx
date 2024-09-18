95c4277ef62f18b54452dfaa1266c0a8fdfcafe3
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-09-18 11:31:56 +0300

    devam

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,31 +1,14 @@
 import { View, Text, Pressable } from "react-native";
+import { UserProvider } from "../context/UserContext";
 import ComponentA from "../components/ComponentA";
-import ImageComp from "../components/ImageComp";
-import ListData from "../components/ListData";
-import HugeComp from "../components/HugeComp";
 
 const Index = () => {
   return (
     <View>
-      <HugeComp></HugeComp>
-      {/* <Text>Index</Text>
-      <ComponentA></ComponentA>
-      
-      <Pressable onPress={() => console.log("pressed")}>
-        <Text style={{ color: "teal" }}>Press Me</Text>
-      </Pressable>
-
-      <Pressable onPressIn={() => console.log("onpress in")}>
-        <Text style={{ color: "coral" }}>Press Me</Text>
-      </Pressable>
-
-      <Pressable onPressOut={() => console.log("onpress out")}>
-        <Text style={{ color: "blue" }}>Press Me</Text>
-      </Pressable>
-
-      <Pressable onLongPress={() => console.log("presse")}>
-        <Text style={{ color: "black" }}>Press Me</Text>
-      </Pressable> */}
+      <Text className="text-red-400">selam</Text>
+      <UserProvider>
+        <ComponentA></ComponentA>
+      </UserProvider>
     </View>
   );
 };
--- src/components/ComponentA.tsx
+++ src/components/ComponentA.tsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react'
 import { View, Text } from 'react-native'
-import React from 'react'
+import { UserContext } from '../context/UserContext'
 
 const ComponentA = () => {
+  const context = useContext(UserContext)
+  const { user, setUser } = context
+
   return (
     <View>
       <Text>ComponentA</Text>
+      <Text>{user}</Text>
     </View>
   )
 }
